2d90437f72ade180409333a2a622144f1d003dac
ridafatima1157 <fatima.rida64000@gmail.com>
2026-05-10 00:50:50 +0500

    updaeee

--- frontend/src/pages/mypage.jsx
+++ frontend/src/pages/mypage.jsx
@@ -72,12 +72,7 @@ const MyPage = () => {
         >
           Click Me
         </button>
-         <button 
-          onClick={() => alert('Hello!')}
-          style={styles.button}
-        >
-          Click Me
-        </button>
+        
         
     
         
