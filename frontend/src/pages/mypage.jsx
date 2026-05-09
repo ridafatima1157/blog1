e170658515467e5ee0833c3089d728c8d0afb6cf
ridafatima1157 <fatima.rida64000@gmail.com>
2026-05-09 22:35:23 +0500

    updaeee

--- frontend/src/pages/mypage.jsx
+++ frontend/src/pages/mypage.jsx
@@ -84,6 +84,12 @@ const MyPage = () => {
         >
           Click Me
         </button>
+        <button 
+          onClick={() => alert('Hello!')}
+          style={styles.button}
+        >
+          Click Me
+        </button>
       </main>
 
       <footer style={styles.footer}>
