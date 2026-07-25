79bfe856a3cc58210baf2b22b313bbb75142f644
ridafatima1157 <fatima.rida64000@gmail.com>
2026-07-25 17:51:05 +0500

    fix: update GitHubConnectCallback component and add vercel.json rewrite rule

--- frontend/src/pages/mypage.jsx
+++ frontend/src/pages/mypage.jsx
@@ -40,33 +40,6 @@ const MyPage = () => {
         >
           Click Me-againnn
         </button>
-    
-      
-       <button 
-         
-          onClick={() => alert('Hello!')}
-          style={styles.button}
-        >
-          Click Me-againnn
-        </button>
-         <button 
-         
-          onClick={() => alert('Hello!')}
-          style={styles.button}
-        >
-          Click Me-againnn
-        </button>
-        
-       
-       
-       
-       
-         
-
-        
-        
-    
-        
         
       </main>
 
